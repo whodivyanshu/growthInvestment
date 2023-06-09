073250ad089e88242ef238af91b76967787eda91
DivyanshuLohar <divyanshulohar2002@gmail.com>
2023-06-10 02:39:20 +0530

    commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,87 @@
+"use client";
+import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
+import styles from './navbar.module.css';
+import Logo from "public/logo.png";
+
+const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+    const [showDrawer, setShowDrawer] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 800) {
+                setShowMenu(true);
+            } else {
+                setShowMenu(false);
+            }
+            setShowDrawer(false); // Close the drawer on resize
+        };
+    
+        handleResize(); // Check initial screen size
+    
+        window.addEventListener("resize", handleResize);
+    
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    const toggleMenu = () => {
+        // setShowMenu(!showMenu);
+        setShowDrawer(!showDrawer);
+    };
+
+    return (
+        <div className={styles.navbar}>
+            <div className={styles.navbar_logo}>
+                <Image src={Logo} width={120} height={50} alt="growth" />
+            </div>
+            {showMenu ? (
+                <div className={styles.menuButton} onClick={toggleMenu}>
+                    <Image width="20" height="20" src="https://img.icons8.com/ios-filled/50/4E0668/menu--v1.png" alt="menu--v1"/>
+                </div>
+            ) : (
+                <ul className={styles.navbarItems}>
+                    <li className={styles.navbarItem}>Home</li>
+                    <li className={styles.navbarItem}>Investments</li>
+                    <li className={styles.navbarItem}>FAQ</li>
+                    <li className={styles.navbarItem}>About Us</li>
+                    <li className={styles.navbarItem}>
+                        <button className={styles.navbtn1}>Refer and Earn</button>
+                    </li>
+                    <li className={styles.navbarItem}>
+                        <Image src="https://img.icons8.com/ios/90/000000/headset--v1.png" width={30} height={30} alt='customer care' />
+                    </li>
+                    <li className={styles.navbarItem}>
+                        <button className={styles.navbtn}>Sign In</button>
+                    </li>
+                </ul>
+            )}
+            {showDrawer && (
+                <div className={styles.drawer}>
+                    <div className={styles.close} onClick={toggleMenu}>
+                        <Image style={{cursor: "pointer"}} src="https://img.icons8.com/ios/90/000000/multiply.png" width={30} height={30} alt="close" />
+                    </div>
+                    <ul className={styles.drawerItems}>
+                        <li className={styles.drawerItem}>Home</li>
+                        <li className={styles.drawerItem}>Investments</li>
+                        <li className={styles.drawerItem}>FAQ</li>
+                        <li className={styles.drawerItem}>About Us</li>
+                        <li className={styles.drawerItem}>
+                            <button className={styles.drawerbtn1}>Refer and Earn</button>
+                        </li>
+                        <li className={styles.drawerItem}>
+                            <Image src="https://img.icons8.com/ios/90/4E0668/headset--v1.png" width={30} height={30} alt='customer care' />
+                        </li>
+                        <li className={styles.drawerItem}>
+                            <button className={styles.drawerbtn}>Sign In</button>
+                        </li>
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Navbar;
