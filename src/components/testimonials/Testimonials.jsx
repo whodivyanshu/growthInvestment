352a5aa12c00e35ca157e99431f711e166cdd7f6
Aditya Sharma <sharma.aditya19@outlook.com>
2023-06-11 08:59:47 +0530

    "key" issue debugging

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -32,8 +32,8 @@ const Testimonials=()=> {
     <div className={styles.cover}>
         <h1 className={styles.heading}>Happy Aasthians</h1>
         <div className={styles.reviewContainer}>
-          {[0,1,2,3,4].map((key)=>(
-            <ReviewTile index={key}/>
+          {[0,1,2,3,4].map(i=>(
+            <ReviewTile key={i}/>
           ))}
 
     </div>
